8b5c3f40e999810f8e968f18485a4d43d35494a8
sirikasamkit <sirikasamkit.t@ku.th>
2026-01-23 19:24:23 +0700

    update

--- register.php
+++ register.php
@@ -6,6 +6,62 @@
     <title>Register New gen new me</title>
 </head>
 <body>
-    
+    <div class="wrapper">
+       <span class="icon-close">
+        <ion-icon name="close"></ion-icon>
+      </span>
+
+       <div class="form-box login">
+             <h2>Login</h2>
+             <form action="#">
+              <div class="input-box">
+                <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                <input type="email" id="login-email" required>
+                <label>Email</label>
+              </div>
+              <div class="input-box">
+                <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                <input type="password" id="login-password" required>
+                <label>Password</label>
+              </div>
+              <div class="remember-forgot">
+                <label><input type="checkbox"> remember me</label>
+                <a href="#">Forgot Password?</a>
+              </div>
+              <button type="submit" id="login-btn" class="btn">Login</button>
+              <div class="login-register">
+                <p>Don't have an account? <a href="#"class="register-link">Register</a></p>
+              </div>
+             </form>
+       </div>
+
+       <div class="form-box register">
+        <h2>Registration</h2>
+        <form action="#">
+          <div class="input-box">
+            <span class="icon"><ion-icon name="person"></ion-icon></ion-icon></span>
+            <input type="text" required>
+            <label>Username</label>
+          </div>
+         <div class="input-box">
+           <span class="icon"><ion-icon name="mail"></ion-icon></span>
+           <input type="email" required>
+           <label>Email</label>
+         </div>
+         <div class="input-box">
+           <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+           <input type="password" required>
+           <label>Password</label>
+         </div>
+         <div class="remember-forgot">
+           <label><input type="checkbox"> I agree to the terms & conditions</label>
+         </div>
+         <button type="submit" class="btn">Register</button>
+         <div class="login-register">
+           <p>Already have an account? <a href="#"class="login-link">login</a></p>
+         </div>
+        </form>
+  </div>
+   </div>
 </body>
 </html>
